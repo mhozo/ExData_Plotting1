262e4773630438eeb12fdf2941a890a592b911e6
mhozo <makhozo@gmail.com>
2014-10-12 18:25:04 -0500

    Pushed

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-#Plots plot1.png, course project 1
+#Plots plot2.png, course project 1
 powert <- read.table("household_power_consumption.txt", sep=";", header=T, colClasses=c("character", "character", rep("numeric", 7)), na.strings="?") 
 #read initial data
 
@@ -8,11 +8,15 @@ powert$Date <- as.Date(powert$Date, format="%d/%m/%Y")
 powert <- powert[powert$Date >= as.Date("2007-02-01") & powert$Date<=as.Date("2007-02-02"),] 
 #subset from only these two dates
 
-png(plot1.png, height=480, width=480) 
+powert$Date <- as.POSIXlt(paste(as.Date(powert$Date, format="%d/%m/%Y"), powert$Time, sep=" "))
+
+
+png(plot2.png, height=480, width=480) 
 #size
 
-hist(powert$Global_active_power, breaks=12, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red") 
-#plot the histogram
+plot(powert$Date, powert$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 
-dev.copy(png, file="plot1.png")
+     #plot the graph
+
+dev.copy(png, file="plot2.png")
 dev.off() #turn off graphic device
